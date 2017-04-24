412ab47c473dceb62283a2554a2c881b71014fa5
vagrant <vagrant@precise32.(none)>
2017-04-24 02:01:57 +0000

    fix fb login

--- checkstub.py
+++ checkstub.py
@@ -15,7 +15,7 @@ from cred import secret_key, location_id, sq_access_token
 app = Flask(__name__)
 
 # engine and db connection
-engine = create_engine('postgresql://daniel: ByeByeMan@localhost/stub')
+engine = create_engine('postgresql://daniel:Seven11ok@localhost/stub')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -42,38 +42,30 @@ def fbconnect():
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+    
     access_token = request.data
-    print "access token received %s " % access_token
-
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.4/me"
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
     # strip expire tag from access token
-    token = result.split("&")[0]
-    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    token = json.loads(result.split("&")[0])
+    token = token["access_token"]
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
     data = json.loads(result)
-#    login_session['provider'] = 'facebook'
-#    login_session['username'] = data["name"]
-
-    # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
-    stored_token = token.split("=")[1]
-    login_session['access_token'] = stored_token
+    print data
+    
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
     login_session['facebook_id'] = data["id"]
-    login_session['state'] = state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                                             for x in xrange(32)
-                                             )
 
     # see if user exists
     try:
@@ -87,23 +79,19 @@ def fbconnect():
         session.commit()
         login_session['email'] = email
 
+    
     # Get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
-
+    print data
     login_session['picture'] = data["data"]["url"]
 #    login_session['user_id'] = user_id
 
-    output = ''
+    return "logged in"
 
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 200px; height: 200px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-    return output
-
-
+    
 @app.route('/logout/')
 def logout():
     if check_login_and_csrf() == True:
@@ -254,7 +242,6 @@ def check_stub():
             return redirect(url_for('my_home'))
     if 'facebook_id' in login_session and 'state' in login_session:
         pic = login_session['picture']
-        print pic
         return render_template('loggedinfront.html',pic=pic)
     return render_template('checkstub.html',state=state)
 
